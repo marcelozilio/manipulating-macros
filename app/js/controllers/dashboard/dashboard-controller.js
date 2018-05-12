4ea39d8a4b9540102a9ff388e2b4a3b2da75c8ad
Marcelo Zilio Correa <marcelo.zilio@hotmail.com>
2018-05-12 00:40:53 -0300

    Criação da estrutura do front-end e LICENSE (MIT)

---
+++ app/js/controllers/dashboard/dashboard-controller.js
@@ -0,0 +1,14 @@
+﻿(function () {
+    'use strict';
+    angular.module('manipulating-macros').controller('DashboardController', DashboardController);
+
+    function DashboardController() {
+        var vm = this;
+        
+        activate();
+
+        function activate() {
+
+        }
+    };
+})();
---
+++ app/js/routes.js
@@ -0,0 +1,22 @@
+﻿(function () {
+    'use strict';
+
+    angular.module('manipulating-macros').config(function ($routeProvider) {
+        $routeProvider
+            .when('/', {
+                controller: 'DashboardController',
+                templateUrl: 'pages/dashboard/dashboard.html',
+                controllerAs: 'vm'
+            })
+            .when('/login', {
+                controller: 'LoginController',
+                templateUrl: 'pages/login/login.html',
+                controllerAs: 'vm'
+            })
+            .when('/registrar', {
+                controller: 'RegistrarController',
+                templateUrl: 'pages/login/registrar.html',
+                controllerAs: 'vm'
+            });
+    });
+})();
